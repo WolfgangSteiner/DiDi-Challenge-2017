73e96b8c59cd5405c7f14014f73a7f14d3a65b5c
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-03-23 12:43:17 +0000

    rename test.py -> render_birdseye_view.py, new O(n) algorithm

---
+++ render_birdseye_view.py
@@ -0,0 +1,53 @@
+import pykitti
+import numpy as np
+import cv2
+import moviepy.editor as mpy
+import imageutils
+from Utils import progress_bar
+basedir = './kitti'
+date = '2011_09_26'
+drive = '0001'
+
+# The range argument is optional - default is None, which loads the whole dataset
+data = pykitti.raw(basedir, date, drive)
+data.load_velo()
+
+def coord_for_point(point):
+    x = point[0]
+    y = point[1]
+
+    if x < 0 or x >= 70 or y < -40 or y >= 40:
+        return None
+    else:
+        return 800 - 1 - int((y + 40) * 10), 700 - 1 - int(x * 10)
+
+
+def create_birds_eye_view(velo):
+    w,h = 800,700
+    output = np.zeros((h,w))
+
+    for p in velo:
+        c = coord_for_point(p)
+
+        if not c is None:
+            x, y = c
+            output[y,x] = max(output[y,x], p[3])
+
+    output = (output * 255).astype(np.uint8)
+    output = imageutils.expand_channel(output)
+    return output
+
+
+frames = []
+
+for i,d in enumerate(data.velo):
+    progress_bar(i, len(data.velo))
+    frame = create_birds_eye_view(d)
+    frames.append(frame)
+
+if len(frames) > 1:
+    print("Creating video...")
+    clip = mpy.ImageSequenceClip(frames, fps=10)
+    clip.write_videofile("test.mp4")
+else:
+    cv2.imwrite("out.png", frames[0])
