5610d85ef140847c8724ea3dc6b1d35e4a80f349
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-03-29 13:29:56 +0200

    fix numpy include path

--- MV3DFeatures/setup.py
+++ MV3DFeatures/setup.py
@@ -1,8 +1,13 @@
 from distutils.core import setup, Extension
+import numpy as np
+
+print(np.get_include())
+
 
 ext = Extension(
     'MV3DFeatures._MV3DFeatures',
     sources = ['src/_MV3DFeatures.cpp'],
+    include_dirs=[np.get_include()],
     libraries=["boost_python"])
 
 setup(
@@ -11,4 +16,3 @@ setup(
     version='0.1.0',
     packages=['MV3DFeatures'],
     ext_modules = [ext])
-
