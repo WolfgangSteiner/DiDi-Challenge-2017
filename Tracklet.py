5c544df6282caffb9aa21aa999f605718bd6fe60
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-03-30 23:57:28 +0200

    add module to read kitti tracklet xml files

---
+++ Tracklet.py
@@ -0,0 +1,105 @@
+import xml.etree.ElementTree as ElementTree
+import numpy as np
+from math import cos, sin
+from VectorMath import *
+
+class Pose(object):
+    def __init__(self):
+        self.translation = None
+        self.rotation = None
+        self.state = None
+        self.occlusion = None
+        self.occlusion_kf = None
+        self.truncation = None
+        self.amt_occlusion = None
+        self.amt_occlusion_kf = None
+        self.amt_border_l = None
+        self.amt_border_r = None
+        self.amt_border_kf = None
+
+
+class TrackletObject(object):
+    def __init__(self):
+        self.type = None
+        self.size = None
+        self.first_frame = None
+        self.poses = []
+
+
+    def __repr__(self):
+        return "TrackletObject: Type: %s Size: %s first_frame: %d, poses: %s" % (self.type, self.size, self.first_frame, self.poses)
+
+
+    def pose_for_frame(self, frame_idx):
+        if frame_idx < self.first_frame or frame_idx >= self.first_frame + len(self.poses):
+            return None
+        else:
+            return self.poses[frame_idx - self.first_frame]
+
+
+    def has_pose_for_frame(self, frame_idx):
+        return not self.pose_for_frame(frame_idx) is None
+
+
+    def bounding_box_for_frame(self, frame_idx):
+        pose = self.pose_for_frame(frame_idx)
+        bbox = np.zeros((8,4))
+        h, w, l = self.size
+        bbox[0,:] = [ l/2, -w/2, -h/2, 1.0]
+        bbox[1,:] = [ l/2,  w/2, -h/2, 1.0]
+        bbox[2,:] = [-l/2,  w/2, -h/2, 1.0]
+        bbox[3,:] = [-l/2, -w/2, -h/2, 1.0]
+        bbox[4:8,:] = bbox[0:4,:] + [ 0.0, 0.0, h, 0.0]
+        rz = pose.rotation[2]
+        t = Transformation()
+        t.add_transformation(matrix_rotation_z(rz))
+        t.add_transformation(matrix_translate(pose.translation))
+        return t.transform(bbox)
+
+
+def get_float(node, tag):
+    return float(node.find(tag).text)
+
+
+def get_int(node, tag):
+    return int(node.find(tag).text)
+
+
+def parse_tracklets(file_name):
+    tree = ElementTree.parse(file_name)
+    root = tree.getroot()
+    tracklets_node = root.find("tracklets")
+    tracklets = []
+
+    for n in tracklets_node.findall("item"):
+        tracklet = TrackletObject()
+        tracklet.type = n.find("objectType").text
+        tracklet.size = np.array((get_float(n, "h"), get_float(n, "w"), get_float(n, "l")), np.float32)
+        tracklet.first_frame = get_int(n, "first_frame")
+        tracklets.append(tracklet)
+
+        for p in n.findall("poses/item"):
+            pose = Pose()
+            pose.translation = np.array((get_float(p,"tx"), get_float(p,"ty"), get_float(p,"tz")), np.float32)
+            pose.rotation = np.array((get_float(p,"rx"), get_float(p,"ry"), get_float(p,"rz")), np.float32)
+            pose.state = get_int(p,"state")
+            pose.occlusion = get_int(p,"occlusion")
+            pose.occlusion_kf = get_int(p,"occlusion_kf")
+            pose.amt_occlusion = get_float(p,"amt_occlusion")
+            pose.amt_occlusion_kf = get_float(p,"amt_occlusion_kf")
+            pose.amt_border_l = get_float(p,"amt_border_l")
+            pose.amt_border_r = get_float(p,"amt_border_r")
+            pose.amt_border_kf = get_float(p,"amt_border_kf")
+            tracklet.poses.append(pose)
+
+    return tracklets
+
+
+def bounding_boxes_for_frame(tracklets, frame_idx):
+    bboxes = []
+
+    for t in tracklets:
+        if t.has_pose_for_frame(frame_idx):
+            bboxes.append(t.bounding_box_for_frame(frame_idx))
+
+    return bboxes
---
+++ VectorMath.py
@@ -0,0 +1,120 @@
+import numpy as np
+import math
+
+class Transformation(object):
+    def __init__(self):
+        self.m = matrix_identity()
+
+
+    def add_transformation(self, m):
+        self.m = np.matmul(m, self.m)
+
+
+    def transform(self, coords):
+        return np.matmul(coords, self.m.transpose())
+
+
+    def flip_xy(self):
+        self.add_transformation(matrix_flip_xy())
+
+
+    def mirror_x(self):
+        self.add_transformation(matrix_mirror_x())
+
+
+    def mirror_y(self):
+        self.add_transformation(matrix_mirror_y())
+
+
+    def mirror_xy(self):
+        self.add_transformation(matrix_mirror_x())
+        self.add_transformation(matrix_mirror_y())
+
+
+    def translate(self, *args):
+        self.add_transformation(matrix_translate(*args))
+
+
+    def scale(self, s):
+        self.add_transformation(matrix_scale(s))
+
+
+
+def matrix_identity():
+    m = np.zeros((4,4), np.float32)
+    np.fill_diagonal(m, 1.0)
+    return m
+
+
+def matrix_zero():
+    m = np.zeros((4,4), np.float32)
+    return m
+
+
+def matrix_rotation_z(rz):
+    m = matrix_identity()
+    m[0:2,0] = [math.cos(rz), -math.sin(rz)]
+    m[0:2,1] = [math.sin(rz), math.cos(rz)]
+    return m
+
+
+def matrix_translate(*args):
+    if len(args) == 1:
+        tx, ty, tz = args[0]
+    else:
+        tx, ty, tz = args
+
+    m = matrix_identity()
+    m[0,3] = tx
+    m[1,3] = ty
+    m[2,3] = tz
+    return m
+
+
+def matrix_flip_xy():
+    m = matrix_zero()
+    m[0,1] = 1.0
+    m[1,0] = 1.0
+    m[2,2] = 1.0
+    m[3,3] = 1.0
+    return m
+
+
+def matrix_mirror_y():
+    m = matrix_identity()
+    m[1,1] = -1.0
+    return m
+
+
+def matrix_mirror_x():
+    m = matrix_identity()
+    m[0,0] = -1.0
+    return m
+
+
+def matrix_scale(s):
+    m = matrix_identity()
+    m[0,0] = s
+    m[1,1] = s
+    m[2,2] = s
+    return m
+
+
+def combine_tranformations(*args):
+    m = matrix_identity();
+    for t in args:
+        m = np.matmul(t, m)
+    return m
+
+
+def assert_equal(a, b):
+    assert np.all(np.isclose(a,b))
+
+
+if __name__ == "__main__":
+    v = np.ones(4)
+    t = Transformation()
+    t.add_transformation(matrix_translate(1,2,3))
+    assert_equal(t.transform(v), [2,3,4,1])
+    t.add_transformation(matrix_scale(0.1))
+    assert_equal(t.transform(v), [0.2, 0.3, 0.4, 1.0])
