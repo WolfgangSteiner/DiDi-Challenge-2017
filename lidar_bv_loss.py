5addd0e115129e7544cde8b6b5eb81dea63738af
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-04-09 01:39:19 +0200

    update loss function

--- lidar_bv_loss.py
+++ lidar_bv_loss.py
@@ -39,64 +39,51 @@ def loc_loss(y_true, y_pred):
     return tf.reduce_sum(l1_loss, axis=-1)
 
 
+def safe_log(y, eps=1e-15):
+    return tf.log(tf.maximum(eps, y))
+
+
 def log_loss(y_true, y_pred):
     '''
-    Compute the softmax log loss.
-    Arguments:
-        y_true (nD tensor): A TensorFlow tensor of any shape containing the ground truth data.
-            In this context, the expected tensor has shape (batch_size, #boxes, #classes)
-            and contains the ground truth bounding box categories.
-        y_pred (nD tensor): A TensorFlow tensor of identical structure to `y_true` containing
-            the predicted data, in this context the predicted bounding box categories.
-    Returns:
-        The softmax log loss, a nD-1 Tensorflow tensor. In this context a 2D tensor
-        of shape (batch, n_boxes_total).
-
-    References:
-        https://github.com/pierluigiferrari/ssdkeras/blob/master/keras_ssd_loss.py
+    Calculate the log loss between y_true and y_pred. y_pred is considered to be the output
+    of a sigmoid activation function.
     '''
-    # Make sure that `y_pred` doesn't contain any zeros (which would break the log function)
-    y_pred = tf.maximum(y_pred, 1e-15)
-    # Compute the log loss
-    log_loss = -tf.reduce_sum(y_true * tf.log(y_pred))
-    return log_loss
+    eps = 1e-15
+    return -y_true * safe_log(y_pred) - (1.0 - y_true) * safe_log(1.0 - y_pred)
 
 
 def multitask_loss(y_true, y_pred):
-    num_classes = 1
+    alpha = 0.125
     nx = 32
     ny = 32
+    num_classes = 1
     num_parameters = 6
     y_shape = (-1,nx*ny,num_classes+num_parameters)
     y_true = tf.reshape(y_true, y_shape)
     y_pred = tf.reshape(y_pred, y_shape)
     positives = y_true[:,:,0]
-
-    # number of positive boxes per example
-    # size (batch_size)
-    n_positives = tf.reduce_sum(positives,axis=-1)
+    negatives = 1.0 - positives
 
     y_true_conf = y_true[:,:,0]
     y_pred_conf = tf.sigmoid(y_pred[:,:,0])
+    n_positives = tf.reduce_sum(positives,axis=-1)
+    n_neg_examples_to_keep = tf.to_int32(tf.reduce_sum(n_positives) * 3)
 
-    confidence_loss = -y_true_conf * tf.log(tf.maximum(1e-15, y_pred_conf)) \
-        - (1 - y_true_conf) * tf.log(tf.maximum(1e-15, 1.0 - y_pred_conf))
+    # calculate the log loss
+    positive_classification_loss = -positives * safe_log(y_pred_conf)
+    negative_classification_loss = -negatives * safe_log(1.0 - y_pred_conf)
+    negative_classification_loss_1d = tf.reshape(negative_classification_loss, (-1,))
+
+    values, indices = tf.nn.top_k(negative_classification_loss_1d, n_neg_examples_to_keep, False)
+    negative_classification_loss_1d = tf.scatter_nd(indices, values, tf.shape(negative_classification_loss_1d))
+    negative_classification_loss = tf.reshape(negative_classification_loss, (-1,nx*ny))
+
+    confidence_loss = positive_classification_loss + negative_classification_loss
     confidence_loss = tf.reduce_sum(confidence_loss, axis=-1)
 
     localization_loss = loc_loss(y_true, y_pred) * positives
     localization_loss = tf.reduce_sum(localization_loss, axis=-1)
 
-
-    total_loss = (1.0 / tf.maximum(n_positives, 1)) * (confidence_loss + localization_loss)
-
-#    zero_loss = tf.zeros_like(localization_loss)
-#    total_loss = tf.where(tf.equal(n_positives, 0), zero_loss, total_loss)
+    total_loss = (1.0 / tf.maximum(n_positives, 1)) * (confidence_loss + alpha * localization_loss)
 
     return total_loss
-
-    #loc_loss = smooth_L1_loss(y_true_params, y_pred_params)
-    #confidence_loss = log_loss(y_true[:,:,0], y_pred[:,:,0])
-    #if n_positives == 0:
-    #    return tf.zeros_like(y_true[:,0,0])
-    #else:
-    #    return
